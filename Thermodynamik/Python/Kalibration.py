60f8b5ec4bd5e7ed7d38543d51c43f7b4768c00d
maoam13 <moritz.berger@rwth-aachen.de>
2017-03-15 23:13:56 +0100

    Graph Kalibration

--- Thermodynamik/Python/Kalibration.py
+++ Thermodynamik/Python/Kalibration.py
@@ -19,6 +19,7 @@ T_E = M_S_1[:, 2]
 
 M_S_2 = p.lese_lab_datei('lab/Temperatur_siedend.lab')
 T_S = M_S_2[:, 2]
+T_S = T_S[789:1089]
 
 def func(T_E, T_S):
     #Mittelwert und Standardabweichung für Temperaturen
@@ -36,9 +37,25 @@ def func(T_E, T_S):
     sig_T_0 = np.sqrt(sig_m**2 * T_S_std**2)
     return m, sig_m, T_0, sig_T_0
 
-m, sig_m, T_0, sig_T_0 = func(T_E, T_S[789:1089])
+m, sig_m, T_0, sig_T_0 = func(T_E, T_S)
 
 print("Steigung der Geraden: ", m, " +/- ", sig_m)
 print("Offset der Geraden: ", T_0, " +/- ", sig_T_0)
-
 print("Laufzeit: {0:9.2f} Sekunden".format(timeit.default_timer()-start_time))
+
+def graphen():
+    x=np.linspace(-20,120,100)
+    y=m*x+T_0
+    plt.figure()
+    plt.plot(x,y)
+    x2=[np.mean(T_E),np.mean(T_S)]
+    y2=[0,100]
+    err=[np.std(T_E),np.std(T_S)]
+    plt.errorbar(x2,y2,yerr=err,fmt='o')
+    plt.xlabel('T(gemessen)/C')
+    plt.ylabel('T(erwartet)/C')
+    plt.title('Temperatur')
+    plt.figtext(0.2,0.7,'Steigung: '+ str(np.round(m,3)) + ' +/- '+ str(np.round(sig_m,3))+'\n'
+                +'Offset: '+ str(np.round(T_0,4)) + ' +/- '+str(np.round(sig_T_0,4)))
+    
+graphen()
