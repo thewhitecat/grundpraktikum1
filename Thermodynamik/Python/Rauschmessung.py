1b9a2e256b27cc7d293d78e7b3e0966dd5d1d812
maoam13 <moritz.berger@rwth-aachen.de>
2017-03-15 16:37:32 +0100

    Add files via upload

--- Thermodynamik/Python/Rauschmessung.py
+++ Thermodynamik/Python/Rauschmessung.py
@@ -7,16 +7,21 @@ Created on Wed Mar 15 14:06:56 2017
 
 import Praktikum as p
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import scipy.constants as const
 import math
 import numpy as np
 
-data = p.lese_lab_datei('lab/Rauschmessungen.lab')
+data = p.lese_lab_datei('lab/Druck_Temperatur_Rauschmessung.lab')
 T = data[:,2]
 p = data[:,4]
 
-#Gibt aus:
-#Mittelwert, Standardabweichung, Ferhler auf Mittelwert
 def mean(data):
+    '''
+    Eingabe: Eindimensionales Array mit Einzelmessungen
+    Gibt aus:
+    Mittelwert, Standardabweichung, Ferhler auf Mittelwert
+    '''
     mean = 0
     N = len(data)
     for i in range(N):
@@ -34,3 +39,15 @@ Tm,Tstd,Tf = mean(T)
 print "Temperatur:",Tm," Fehler:", Tstd
 pm,pstd,pf = mean(p)
 print "Druck:",pm," Fehler:", pstd
+
+def histogramm(data,title,xlabel):
+    ax = plt.subplot(1,1,1)
+    plt.ylabel("#")
+    plt.xlabel("xlabel")
+    mu,sigma,f = mean(data)
+    norm = np.full(data.size,1.0/data.size)
+    n,bins,patches = ax.hist(data,weights=norm,rwidth=2)
+    y = mlab.normpdf(bins, mu, sigma)
+    ax.plot(bins, y, 'r--', linewidth=2)
+    
+histogramm(T,"test","teset")
