598222f42eb9a406e5caa5cb9ebe297ac0984f71
Runcorn <klausreturns11@web.de>
2017-09-08 14:29:38 +0200

    fd

---
+++ grundpraktikum2/Optik 2/Kalibration_A.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 08 14:12:28 2017
+
+@author: Tim
+"""
+
+import numpy as np
+import Praktikum
+
+
+data = np.genfromtxt("Kalibration_A.txt",delimiter = ",",skip_header = 1)
+m = np.array([data[]])
+
+
+
+
+
+
+
+
+
+
