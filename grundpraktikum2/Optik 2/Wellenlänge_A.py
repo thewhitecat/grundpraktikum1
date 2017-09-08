0d2b896fc65741516cadfd06cf9efa0337510a11
Runcorn <klausreturns11@web.de>
2017-09-08 15:23:01 +0200

    stuff

--- grundpraktikum2/Optik 2/Wellenlänge_A.py
+++ grundpraktikum2/Optik 2/Wellenlänge_A.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 08 14:12:28 2017
+Created on Fri Sep 08 15:03:47 2017
 
 @author: Tim
 """
@@ -9,15 +9,17 @@ import numpy as np
 import Praktikum
 import matplotlib.pyplot as plt
 
+k = 0.0486685018496
+ek = 0.000179054717282
 
-data = np.genfromtxt("Kalibration_A.txt",delimiter = ",",skip_header = 1)
+data = np.genfromtxt("Wellen_A.txt",delimiter = ",",skip_header = 1)
 m, s = data[:,0], data[:,1]
 
 s_error = np.full(len(s),0.01/np.sqrt(12))
 
 a,ea,b,eb,chiq,cov = Praktikum.lineare_regression(m,s,s_error)
 chiq_ndof = chiq/(len(s)-2)
-x_axis = np.linspace(0,85,100)
+x_axis = np.linspace(0,105,150)
 
 
 plt.figure()
@@ -26,7 +28,7 @@ ax1 = plt.subplot(211)
 plt.errorbar(m,s,s_error,linestyle = 'None',marker = '.')
 plt.plot(x_axis,a*x_axis+b)
 plt.ylabel('s/mm')
-plt.figtext(0.15,0.70,' a = {}$\pm${} \n b = {}$\pm${} \n $\chi^2/ndof$ = {}'.format(round(a,5),round(ea,5),round(b,5),round(eb,5),round(chiq_ndof,3)))
+plt.figtext(0.15,0.57,' a = {}$\pm${} \n b = {}$\pm${} \n $\chi^2/ndof$ = {}'.format(round(a,5),round(ea,5),round(b,5),round(eb,5),round(chiq_ndof,3)))
 plt.setp(ax1.get_xticklabels(),visible=False)
 
 ax2 = plt.subplot(212,sharex=ax1)
@@ -36,23 +38,12 @@ plt.xlabel('m')
 plt.ylabel('Residuen')
 plt.show()
 
+# in nm
+a = a*1e6
+ea = ea*1e6
 
-l = 632.8e-9
-el = 0.1e-9
+l = 2*k*abs(a)
+el_stat = l * ea/abs(a)
+el_syst = l * ek/k
 
-a = a*1e-3
-ea = ea*1e-3
-
-k = l/(2*a)
-ek = k*np.sqrt((el/l)**2+(ea/a)**2)
-
-print k,ek
-
-
-
-
-
-
-
-
-
+print l,el_stat, el_syst
