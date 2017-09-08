992bc561260ae8f9e26c6e44cc124a8d38d84321
thewhitecat <sebastian.siebert@rwth-aachen.de>
2017-09-08 15:35:39 +0200

    stuff

--- grundpraktikum2/Optik 2/Python/kalibration_seb.py
+++ grundpraktikum2/Optik 2/Python/kalibration_seb.py
@@ -13,9 +13,9 @@ import Praktikum as p
 laser = 632.8e-9
 sig_laser = 0.1e-9
 
-m, s = np.genfromtxt("..\Kalibration_A.txt", skip_header=1, delimiter=",", unpack=True)
+m, s = np.genfromtxt("..\Kalibration_B.txt", skip_header=1, delimiter=",", unpack=True)
 s*=1e-3
-sig_s = 0.005e-3/np.sqrt(12)
+sig_s = 0.01e-3/np.sqrt(12)
 
 # 2d = m lambda
 # d = ks
@@ -51,9 +51,10 @@ plt.xlabel("m")
 #
 ############################
 
-m, s = np.genfromtxt("", skip_header=1, delimiter=",", unpack=True)
+m, s, s2 = np.genfromtxt("..\Wellenlaenge_B.txt", skip_header=1, delimiter=",", unpack=True)
 s*=1e-3
-
+#s = s[0:-1]
+m = m[0:len(s)]
 # 2 ks = lambda m
 
 y = 2*k*s
@@ -62,6 +63,19 @@ ey = y* np.sqrt( (ek/k)**2 + (sig_s/s)**2 )
 
 a, ea, b, eb, chi2, corr = p.lineare_regression(m, y, ey)
 
+plt.figure(2, [6.5,4.5])
+plt.subplot2grid((6,1),(0,0), rowspan=4)
+plt.errorbar(m, y, yerr=ey, fmt=".")
+plt.plot(m, a*m+b)
+plt.figtext(0.2, 0.65, "2 k s = $\lambda$ m\n$\lambda = ({0:3.2f} \pm {1:3.2f})nm$\nb = $({2:3.2f} \pm {3:3.2f})$\n$\chi^2/ndof$ = {4:3.2f}".format(a*1e9, ea*1e9, b, eb, chi2/(len(m)-2)))
+plt.ylabel("2 k s")
+plt.xlabel("m")
 
+# Residuen
+plt.subplot2grid((6,1),(-2,0), rowspan=2)
+plt.errorbar(m, y - a*m -b, yerr=ey, fmt = ".")
+plt.axhline(linestyle="dashed")
+plt.ylabel("Residuen")
+plt.xlabel("m")
 
 
