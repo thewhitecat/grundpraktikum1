f6cc7ad007213309aef436377d510f5b6c087dee
thewhitecat <sebastian.siebert@rwth-aachen.de>
2017-09-11 14:20:54 +0200

    Bilder

--- grundpraktikum2/Optik 2/Python/kalibration_seb.py
+++ grundpraktikum2/Optik 2/Python/kalibration_seb.py
@@ -14,9 +14,9 @@ laser = 632.8e-6
 sig_laser = 0.1e-6
 
 
-m, s = np.genfromtxt("Kalibration_A.txt", skip_header=1, delimiter=",", unpack=True)
+m, s = np.genfromtxt("Kalibration_B.txt", skip_header=1, delimiter=",", unpack=True)
 #s*=1e-3
-sig_s = 0.005/np.sqrt(12)
+sig_s = 0.015/np.sqrt(12)
 
 
 # 2d = m lambda
@@ -35,7 +35,7 @@ plt.figure(1, [6.5,4.5])
 plt.subplot2grid((6,1),(0,0), rowspan=4)
 plt.errorbar(m, y, yerr=sig_y, fmt=".")
 plt.plot(m, a*m+b)
-plt.figtext(0.2, 0.65, "y = m/k\n$k = ({0:3.4f} \pm {1:3.4f})$\nb = $({2:3.2f} \pm {3:3.2f})$\n$\chi^2/ndof$ = {4:3.2f}".format(k, ek, b, eb, chi2/(len(m)-2)))
+plt.figtext(0.2, 0.65, "2s/$\lambda$ = m/k + b\n$k = ({0:3.4f} \pm {1:3.4f})$\nb = $({2:3.2f} \pm {3:3.2f})$\n$\chi^2/ndof$ = {4:3.2f}".format(k, ek, b, eb, chi2/(len(m)-2)))
 plt.ylabel("2s/$\lambda$")
 plt.xlabel("m")
 
@@ -54,7 +54,7 @@ plt.xlabel("m")
 ############################
 
 
-m, s = np.genfromtxt("Wellen_A.txt", skip_header=1, delimiter=",", unpack=True)
+m, s, s2 = np.genfromtxt("Wellenlaenge_B.txt", skip_header=1, delimiter=",", unpack=True)
 #s*=1e-3
 
 m = m[0:len(s)]
@@ -72,9 +72,16 @@ plt.figure(2, [6.5,4.5])
 plt.subplot2grid((6,1),(0,0), rowspan=4)
 plt.errorbar(m, y, yerr=ey, fmt=".")
 plt.plot(m, a*m+b)
-plt.figtext(0.2, 0.65, "2 k s = $\lambda$ m\n$\lambda = ({0:3.1f} \pm {1:3.1f})$ nm\nb = $({2:3.1f} \pm {3:3.1f})$ mm\n$\chi^2/ndof$ = {4:3.2f}".format(a*1e6, ea*1e6, b*1e3, eb*1e3, chi2/(len(m)-2)))
+plt.figtext(0.2, 0.65, "2 k s = $\lambda$ m + b\n$\lambda = ({0:3.1f} \pm {1:3.1f})$ nm\nb = $({2:3.1f} \pm {3:3.1f})$ mm\n$\chi^2/ndof$ = {4:3.2f}".format(a*1e6,ea*1e6, b*1e3, eb*1e3, chi2/(len(m)-2)))
 plt.ylabel("2 k s [mm]")
 plt.xlabel("m")
 
+# Residuen
+plt.subplot2grid((6,1),(-2,0), rowspan=2)
+plt.errorbar(m, y - a*m -b, yerr=ey, fmt = ".")
+plt.axhline(linestyle="dashed")
+plt.ylabel("Residuen [mm]")
+plt.xlabel("m")
 
-ea_sys = a*ek/k
+
+ea_sys =np.abs( a*ek/k)
