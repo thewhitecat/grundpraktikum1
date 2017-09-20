8d849c0439656d01faaf9c5d589420d514cf11de
Runcorn <klausreturns11@web.de>
2017-09-20 19:15:30 +0200

    python

--- grundpraktikum2/Atomphysik/Python/A/Epsilon.py
+++ grundpraktikum2/Atomphysik/Python/A/Epsilon.py
@@ -7,15 +7,21 @@ Created on Wed Sep 20 14:49:19 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
-import Praktikum
+import Praktikum as p
 from scipy.constants import sigma as sigma
+import Histogramme as h
+import Regression as r
 
 #steigungen
-p_schwarz, e_schwarz = 1.481856207e-9, 0.01 * p_schwarz
-p_weis, e_weis = 1.4815e-9, 0.01 * p_weis
-p_messing, e_messing = 1.433e-10, 0.01 * p_messing
-p_spiegel, e_spiegel = 7.874e-11, 0.01 * p_spiegel
+p_schwarz, ep_schwarz, b_schwarz, eb_schwarz = r.Auswertung(h.get_werte('schwarz'),plots = False)
+p_weis, ep_weis, b_weis, eb_weis = r.Auswertung(h.get_werte('weiss'),plots = False)
+p_messing, ep_messing, b_messing, eb_messing = r.Auswertung(h.get_werte('messing'),plots = False)
+p_spiegel, ep_spiegel, b_spiegel, eb_spiegel = r.Auswertung(h.get_werte('spiegel'),plots = False)
 
+p_list = np.array([p_schwarz,p_weis,p_messing,p_spiegel])
+ep_list = np.array([ep_schwarz,ep_weis,ep_messing,ep_spiegel])
+b_list = np.array([b_schwarz,b_weis,b_messing,b_spiegel])
+eb_list = np.array([eb_schwarz,eb_weis,eb_messing,eb_spiegel])
 
 #constants
 kelvin = 273.15
@@ -32,8 +38,15 @@ def get_epsilon_easy(p1,ep1):
     epsilon_syst = epsilon * ek/k
     return epsilon, epsilon_stat, epsilon_syst
 
+print get_epsilon_easy(p_list,ep_list)
 
-
-
+def get_epsilon_hard(p1,ep1,p0,ep0,string):
+    data = h.get_werte(string)
+    T, eT = a*(data[0]+kelvin)+b, data[1]*a+b
     
+    eps = ((p0+p1*(T**4-T0**4))*v*0.15**2)/(np.pi*(0.035/2)**2*(0.023/2)**2*sigma*(T**4-T0**4))
     
+    return eps
+
+print get_epsilon_hard(p_schwarz, ep_schwarz, b_schwarz, eb_schwarz,'schwarz')
+#,get_epsilon_hard('weiss'),get_epsilon_hard('messing'),get_epsilon_hard('spiegel')
--- grundpraktikum2/Atomphysik/Python/A/Regression.py
+++ grundpraktikum2/Atomphysik/Python/A/Regression.py
@@ -20,7 +20,7 @@ schwarz = h.get_werte('schwarz')
 spiegel = h.get_werte('spiegel')
 
 
-def Auswertung(data,i):
+def Auswertung(data,i=0,plots = True):
     #kalib anwenden
     T, eT = a*(data[0]+kelvin)+b, data[1]*a+b
     U, eU = data[2], data[3]
@@ -32,24 +32,32 @@ def Auswertung(data,i):
     chiq_ndof = chiq/(len(U)-2)
     x_axis = np.linspace(T4[0],T4[-1],100)
     
-    #hier plot
-    plt.figure(i)
+    if plots == False:
+        return m,em,q,eq
+    else:
+        #hier plot
+        plt.figure(i)
     
-    ax1 = plt.subplot(211)
-    plt.errorbar(T4,U,xerr=eT4,yerr=eU,linestyle='None')
-    plt.plot(x_axis,m*x_axis+q)
-    
-    ax2 = plt.subplot(212,sharex=ax1)
-    plt.errorbar(T4, (U-m*T4-q) , yerr = np.sqrt(eU**2+m**2*eT4**2), linestyle='None')
-    plt.axhline(0)
-    
-    plt.show()
-    
-    print eq/q, chiq_ndof
-    
-    return m,em
+        ax1 = plt.subplot(211)
+        plt.errorbar(T4,U,xerr=eT4,yerr=eU,linestyle='None')
+        plt.plot(x_axis,m*x_axis+q)
+        plt.setp(ax1.get_xticklabels(),visible=False)
+        plt.ylabel('U/V')
+        plt.figtext(0.15,0.73,' a = ({:.2e} $\pm$ {:.2e})V/K$^4$ \n b = ({:.3f} $\pm$ {:.3f})V \n $\chi^2 /ndof = {:.2f}$'.format(m,em,q,eq,chiq_ndof))
+        
+        ax2 = plt.subplot(212,sharex=ax1)
+        plt.errorbar(T4, (U-m*T4-q) , yerr = np.sqrt(eU**2+m**2*eT4**2), linestyle='None')
+        plt.axhline(0)
+        plt.xlabel('$T^4-T_0^4 / K^4$')
+        plt.ylabel('Residuen')
+        
+        plt.show()
+        
+        print eq/q, chiq_ndof
+        
+        return m,em,q,eq
 
-Auswertung(weis,1)
-Auswertung(messing,2)
-Auswertung(schwarz,3)
-Auswertung(spiegel,4)
+Auswertung(h.get_werte('weiss'),1)
+Auswertung(h.get_werte('messing'),2)
+Auswertung(h.get_werte('schwarz'),3)
+Auswertung(h.get_werte('spiegel'),4)
